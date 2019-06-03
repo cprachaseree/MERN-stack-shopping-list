41a5f4cbb44dfe0af4cc358354a35ceb747e194f
cprachaseree <jasoncprach@gmail.com>
2019-06-03 22:44:29 +0700

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://jason:jason+0624@cluster0-vlylb.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+// BodyParser Middeware
+app.use(bodyParser.json());
+
+app.use(cors());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo using mongoose
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Use routes
+app.use('/api/items', items);
+
+// serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // Set Static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
